e8c317a3310ed43fb1a0e969359cd7a48eb883b3
ipekkocak <ipekkocak@localhost>
2015-11-10 00:25:49 +0200

    EntitiyManager eklendi

---
+++ src/main/java/tr/gov/teias/egitim/adresdefteri/EntityMenagerProducer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.teias.egitim.adresdefteri;
+
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ipekkocak
+ */
+public class EntityMenagerProducer {
+    
+    @PersistenceContext(name = "AdresDefteriPU")
+    private EntityManager entityManager;
+    
+    @Produces
+    public EntityManager produceEntityManager(){
+        return entityManager;
+    }
+            
+    
+}
